345851fbdac23ff19874afabc288d71d7dcc323d
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2022-01-12 17:50:23 -0500

    changes from the backend side

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,9 +1,5 @@
-import datetime as _dt
-
-import pydantic as _pydantic
-
-
-class _UserBase(_pydantic.BaseModel):
+import pydantic
+class _UserBase(pydantic.BaseModel):
     email: str
 
 
@@ -16,28 +12,6 @@ class UserCreate(_UserBase):
 
 class User(_UserBase):
     id: int
-
-    class Config:
-        orm_mode = True
-
-
-class _LeadBase(_pydantic.BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    company: str
-    note: str
-
-
-class LeadCreate(_LeadBase):
-    pass
-
-
-class Lead(_LeadBase):
-    id: int
-    owner_id: int
-    date_created: _dt.datetime
-    date_last_updated: _dt.datetime
 
     class Config:
         orm_mode = True
